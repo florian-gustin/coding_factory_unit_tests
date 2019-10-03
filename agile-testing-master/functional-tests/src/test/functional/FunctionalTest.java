3f5c0bea46f8310d97ce4f1ee90db364013add99
Flow <florian.gustin.barriera@gmail.com>
2019-10-03 10:30:12 +0200

    push accept doc

--- agile-testing-master/functional-tests/src/test/functional/FunctionalTest.java
+++ agile-testing-master/functional-tests/src/test/functional/FunctionalTest.java
@@ -31,23 +31,48 @@ public class FunctionalTest {
      }
 
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
-	@Test
-    public void testHomepage() throws Exception {
-        driver.get("https://www.meetup.com/fr-FR/");
+//	@Test
+//    public void testHomepage() throws Exception {
+//        driver.get("https://www.meetup.com/fr-FR/");
 //		assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
-//        assertThat(driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content"), not("Partagez vos passions et faites bouger votre ville ! Meetup vous aide à rencontrer des personnes près de chez vous, autour de vos centres d'intérêt."));
-        assertThat(driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content"), containsString("Partagez vos passions et faites bouger votre ville"));
-        assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Le monde vous tend les bras");
-        assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/main/div[1]/div/section/div/div[2]/p/span")).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
-        assertEquals(driver.findElement(By.xpath("//*[@id=\"joinMeetupButton\"]")).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
-        assertEquals(driver.findElement(By.xpath("//*[@id=\"joinMeetupButton\"]")).getText(), "Rejoindre Meetup");
-    }
+//        assertThat(driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content"), containsString("Partagez vos passions et faites bouger votre ville"));
+//        assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Le monde vous tend les bras");
+//        assertEquals(driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/main/div[1]/div/section/div/div[2]/p/span")).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
+//        assertEquals(driver.findElement(By.xpath("//*[@id=\"joinMeetupButton\"]")).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
+//        assertEquals(driver.findElement(By.xpath("//*[@id=\"joinMeetupButton\"]")).getText(), "Rejoindre Meetup");
+//    }
 
     @Test
     public void testSearchPage() throws Exception {
         driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
         assertThat(driver.getTitle(), containsString("Nature et aventure"));
         assertThat(driver.findElement(By.xpath("//h1")).getText(), containsString("Nature et aventure"));
+
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"findNavBar\"]")));
+
+        assertNotNull(driver.findElement(By.xpath("//html/body/div[3]/div[1]/div[4]/div[1]/div[1]/form/div[3]/a")));
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"simple-radius\"]")));
+
+        assertNotNull(driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[4]/div[1]/div[1]/form/div[5]/a")));
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"locationSearch\"]")));
+
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"simple-view-selector\"]")));
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"simple-view-selector-group\"]")));
+        assertThat(driver.findElement(By.xpath("//*[@id=\"simple-view-selector-group\"]")).getText(), containsString("Groupes"));
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"simple-view-selector-event\"]")));
+        assertThat(driver.findElement(By.xpath("//*[@id=\"simple-view-selector-event\"]")).getText(), containsString("Calendrier"));
+
+        assertThat(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul/li[2]/a")).getAttribute("class"), containsString("selected"));
+
+        assertNotNull(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul")));
+        assertThat(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul/li[2]/a")).getAttribute("data-copy"), containsString("pertinence"));
+        assertThat(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul/li[4]/a")).getAttribute("data-copy"), containsString("plus récents"));
+        assertThat(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul/li[5]/a")).getAttribute("data-copy"), containsString("nombre de membres"));
+        assertThat(driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div[1]/ul/li[6]/a")).getAttribute("data-copy"), containsString("proximité"));
+
+        assertNotNull(driver.findElement(By.xpath("//*[@id=\"simple-view-selector-event\"]")).getAttribute("href"));
+
+
     }
 
 
